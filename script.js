e36c5902e355d4bafc6f1bd963c8ccba7f8ac8ab
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-30 22:52:31 -0300

    posicionanod divs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const game_container = document.querySelector('.game-container')
 const creatingInitialDivs = () => {
   for (let i = 0; i < 30; i += 1) {
     const new_card = document.createElement('div');
-    new_card.className = 'card';
+    new_card.className = 'card-front';
     game_container.appendChild(new_card);
     const new_card_image = document.createElement('img');
     new_card_image.className = 'card-image';
